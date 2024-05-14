9b7e0c785ac887fd1f41547f5aead3186c3a1791
Gee-tha-S <166114149+Gee-tha-S@users.noreply.github.com>
2024-05-14 20:31:24 +0530

    Update main.py

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -18,6 +18,6 @@ response = requests.post(power_bi_url, headers=headers, data=data)
 # Example usage
 sarif_file = 'path/to/result.sarif'
 df = convert_sarif_to_dataframe(sarif_file)
-power_bi_url = 'your_power_bi_streaming_dataset_url'
+power_bi_url = 'https://api.powerbi.com/beta/b945c813-dce6-41f8-8457-5a12c2fe15bf/datasets/49e96d32-1d52-459a-8e8b-09b82ac161d3/rows?referrer=desktop&key=dwVY70XrRwwCZWJWjJcliwhLGgdqErLna6GVAvaWB4E%2BFL5j4NZ58I4mQD3RwG4fJefm8iDXnM1we72OovnswQ%3D%3D'
 response = send_data_to_power_bi(df, power_bi_url)
 print('Data uploaded to Power BI:', response.status_code)
